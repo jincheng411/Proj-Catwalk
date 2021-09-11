37901beaa580b880f2878e272a2a6001454bf0c2
Alex Lukin <apotropaicowl@gmail.com>
2021-09-11 17:20:18 -0600

    Reviews sub-directory and file wireframes created

---
+++ client/components/RatingsAndReviews/ProductBreakdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductBreakdown extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Product Breakdown</h1>
+      </div>
+    )
+  }
+}
+
+export default ProductBreakdown;
---
+++ client/components/RatingsAndReviews/SingleReviewTile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SingleReviewTile extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Lone Review Tile Here</h1>
+      </div>
+    )
+  }
+}
+
+export default SingleReviewTile;
