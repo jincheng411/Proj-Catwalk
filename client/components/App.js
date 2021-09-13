7ed099539f4e49884184c94723afbbdb8104cc24
Michael Harfenist <hrharfenist@gmail.com>
2021-09-13 11:51:56 -0400

    Revert "added a few lines in App.js, added state and set to all products"
    This reverts commit eede474c28eedcdae37f79fcccc0d0dd98ddc40b.

--- client/components/App.js
+++ client/components/App.js
@@ -7,9 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: [],
-      currentProduct: {},
-      currentProductId: null
+      currentProduct: {}
     }
   }
 
@@ -17,23 +15,11 @@ class App extends React.Component {
     let productId = '37313';
     axios.get(`/api/products/${productId}`).then(({ data }) => {
       this.setState({
-        currentProduct: data,
-        currentProductId: data.id
-      })
-    })
-    axios.get('/api/products').then(({data}) => {
-      this.setState({
-        products: data
+        currentProduct: data
       })
     })
   }
-
-  setProduct(req) {
-    // well.. this will be more of an event handler. Can pinpoint to a an aleady existing product from our state, rather than continuously 'getting'
-  }
-
   render() {
-    //console.log(this.state)
     const { name } = this.props;
     const { currentProduct } = this.state;
     return (
@@ -48,4 +34,3 @@ class App extends React.Component {
 }
 
 export default App;
-
