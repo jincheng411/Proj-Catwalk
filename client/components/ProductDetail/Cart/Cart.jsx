75b98e6e0c9b1bb157796f60b46c3efce0b52fdc
Jin Cheng <jincheng411@gmail.com>
2021-09-14 18:07:15 -0400

    showing proper data on shopping cart

--- client/components/ProductDetail/Cart/Cart.jsx
+++ client/components/ProductDetail/Cart/Cart.jsx
@@ -4,21 +4,16 @@ function Cart({items}) {
   const [amount, setAmount] = useState(0);
   const originalTotal = items.reduce((a, b) => {
     return a + Number(b.style.original_price) * Number(b.quantity);
-  }, 0)
+  }, 0);
   const saleTotal = items.reduce((a, b) => {
-    // let price = b.style.sale_price || b.style.original_price;
     return a + Number(b.style.sale_price || b.style.original_price) * Number(b.quantity);
-  }, 0)
-  // const saleTotal = items.reduce((a, b) => {
-  //   return a.style.sale_price || a.style.default_price + a.style.sale_price || a.style.default_price
-  // }, 0)
+  }, 0);
   return (
     <div className="cart">
       Shopping cart
-      {console.log(items)}
-      {console.log(originalTotal)}
-      {console.log(saleTotal)}
-
+      {items.map(item => {
+        return <p key={item.style.style_id+item.size}>{item.productName} Size: {item.size} Style: {item.style.name} price: {item.style.sale_price || item.style.original_price} * {item.quantity}</p>
+      })}
     </div>
   )
 }
