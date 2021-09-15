a34208bd08677e0ca11f901217789679754ac38e
Alex Lukin <apotropaicowl@gmail.com>
2021-09-15 08:05:36 -0600

    ReviewsCore and SingleReviewTile structures built

--- client/components/RatingsAndReviews/SingleReviewTile.jsx
+++ client/components/RatingsAndReviews/SingleReviewTile.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
-class SingleReviewTile extends React.Component {
-  render () {
-    return (
-      <div>
-        <h1>Lone Review Tile Here</h1>
-      </div>
-    )
-  }
+const SingleReviewTile = (props) => {
+  let currentProductId = props.currentProductId;
+
+  return (
+    <div className="single-review-tile">
+      <h3>Product Id: {currentProductId}</h3>
+      <ul>
+        {props.reviews.map((loneReview) => {
+          return (
+            <div className="lone-review" key={loneReview.review_id}>
+              <h5>Review {loneReview.review_id}</h5>
+              {loneReview.rating}<br></br>
+              {loneReview.reviewer_name}<br></br>
+              {loneReview.date}<br></br>
+              {loneReview.summary}<br></br>
+              {loneReview.body}<br></br>
+              {loneReview.recommend}<br></br>
+              {loneReview.response}<br></br>
+              {loneReview.helpfulness}<br></br>
+            </div>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default SingleReviewTile;
