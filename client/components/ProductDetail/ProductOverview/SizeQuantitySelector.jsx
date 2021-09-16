f43e6160a2fd6e83a2b73f9edb21affe8a62c1e7
Jin Cheng <jincheng411@gmail.com>
2021-09-16 19:46:51 -0400

    complete most part of product overview

--- client/components/ProductDetail/ProductOverview/SizeQuantitySelector.jsx
+++ client/components/ProductDetail/ProductOverview/SizeQuantitySelector.jsx
@@ -3,33 +3,45 @@ import _ from 'underscore';
 import SizeSelector from './SizeSelector';
 import QuantitySelector from './QuantitySelector';
 
+
 function SizeQuantitySelector({ style, addToBag }) {
   const [inventory, setInventory] = useState(1);
   const [quantity, setQuantity] = useState(1);
   const [size, setSize] = useState(null);
+  const [sizeWarning, setSizeWarning] = useState(false);
+  const [isFav, setIsFav] = useState(false);
   function updateInventory(inventory, size) {
     setInventory(inventory);
     setSize(size);
-
+    setSizeWarning(false);
   }
   function updateQuantity(num) {
     console.log(`seleted ${num} items`)
     setQuantity(num);
+  }
+  function addToFav() {
+    setIsFav(!isFav);
   }
 
   function handleOnClick() {
     if(size) {
       addToBag({style, size, quantity})
     } else {
+      setSizeWarning(true);
       console.log('select a size first')
     }
   }
 
   return (
     <div className="selector">
-      <SizeSelector style={style} updateInventory={updateInventory}/>
-      <QuantitySelector inventory={inventory} updateQuantity={updateQuantity}/>
-      <button onClick={handleOnClick}>ADD TO BAG</button>
+      <SizeSelector style={style} updateInventory={updateInventory} sizeWarning={sizeWarning}/>
+      {/* <QuantitySelector inventory={inventory} updateQuantity={updateQuantity}/> */}
+      <div className="selector-add-to-cart">
+      <span onClick={handleOnClick}>ADD TO BAG</span>
+      <span onClick={addToFav}>
+        Favorite {isFav ? <i className="fas fa-heart"></i> : <i className="far fa-heart"></i>}
+      </span>
+      </div>
     </div>
   )
 }
--- client/components/ProductDetail/ProductOverview/SizeSelector.jsx
+++ client/components/ProductDetail/ProductOverview/SizeSelector.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import _ from 'underscore';
 
-function SizeSelector({ style, updateInventory }) {
+function SizeSelector({ style, updateInventory, sizeWarning }) {
   const [selectedSize, setSelectedSize] = useState(null);
   function handleOnClick(quantity, size) {
     updateInventory(quantity, size);
@@ -13,10 +13,10 @@ function SizeSelector({ style, updateInventory }) {
         <p>Select Size</p>
         <p>Size Guide</p>
       </div>
-      <div>
+      <div className={sizeWarning ? 'warning-size' : null}>
         {!_.isEmpty(style) && _.map(style.skus, (value, key, sku) => {
           return <span
-            className={value.size === selectedSize ? 'selected' : ''}
+            className={value.size === selectedSize ? 'selected' : null}
             key={key}
             onClick={() => { handleOnClick(value.quantity, value.size) }}>
             {/* value={[value.size, value.quantity]} */}
