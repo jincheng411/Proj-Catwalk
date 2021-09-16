716ae9466c304f57418364d834c6ea2b68f3a0bc
Jin Cheng <jincheng411@gmail.com>
2021-09-16 13:58:52 -0400

    completed price tag styling

--- client/components/ProductDetail/ProductOverview/PriceTag.jsx
+++ client/components/ProductDetail/ProductOverview/PriceTag.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 function PriceTag({ style }) {
   return (
-    <div>
+    <div className="product-overview-price-tag">
       {style.sale_price ?
-      <div><div style={{textDecoration: 'line-through'}}>$ {style.original_price}</div><div style={{color: "red"}}>$ {style.sale_price}</div></div>
+      <div className="on-sale">
+        <div>$ {style.sale_price}</div>
+        <div>$ {style.original_price}</div>
+      </div>
         :
         <div>$ {style.original_price}</div>}
     </div>
