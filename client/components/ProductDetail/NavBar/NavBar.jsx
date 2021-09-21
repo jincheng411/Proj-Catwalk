252e11222ea6460a6333871463f823ca8184a853
Jin Cheng <jincheng411@gmail.com>
2021-09-20 20:09:56 -0400

    created a navbar

---
+++ client/components/ProductDetail/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div>
+      <div>
+        LOGO
+      </div>
+      {console.log('aaa')}
+      <div>
+        <span>men</span>
+        <span>women</span>
+        <span>kids</span>
+        <span>customize</span>
+        <span>sale</span>
+      </div>
+      <div><i className="fas fa-shopping-bag"></i></div>
+    </div>
+  )
+}
+
+export default NavBar;
